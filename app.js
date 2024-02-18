e7e381cbdd0e5a7aaf12b6fb0e48d6a2027d4d43
sheikhabdullahahmed <sheikhabdullahahmed>
2024-02-19 00:30:40 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// var num = 1 
+// while (num <= 10) {
+//     document.write (num + "<br>")
+//     num++
+// }
+
+
+
+// var num =+ prompt("Enter Your Tabel");
+// var num =10
+// while (num <=100) {
+//     document.write (num + "<br>")
+//     num++
+// } 
+
+
+// var numName =+ prompt("enter your nmber") 
+// var numinput =+ prompt("enter your table")   
+// var i =10; 
+// do {
+//     document.write(numName+ 'x' +i+ '=' +numinput*i +'<br>')
+//     num++
+// }
+// while (i<=numName)
